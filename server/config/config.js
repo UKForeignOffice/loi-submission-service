6a08d830fe1d8a56516cff995615a37b826efd47
Simon Moore <s.moore@kainos.com>
2022-09-08 11:12:25 +0100

    Update to node 18

--- server/config/config.js
+++ server/config/config.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 // var rabbitMq = JSON.parse(env.RABBITMQ);
 var dbConn = process.env.DBCONN;
 var submissionApiUrl = process.env.SUBMISSIONAPIURL;
-var certificatePath = process.env.CASEBOOKCERTIFICATE;
-var keyPath = process.env.CASEBOOKKEY;
 var edmsHost = process.env.EDMS_HOST;
 var edmsBearerToken = process.env.EDMS_BEARER_TOKEN;
 var additionalPaymentApiUrl = process.env.ADDITIONALPAYMENTAPIURL;
+var certificatePath = process.env.NODE_ENV !== 'development' ? process.env.CASEBOOKCERTIFICATE : process.env.CASEBOOKCERTIFICATE.replace(/\\n/gm, '\n');
+var keyPath = process.env.NODE_ENV !== 'development' ? process.env.CASEBOOKKEY : process.env.CASEBOOKKEY.replace(/\\n/gm, '\n');
 var hmacKey = process.env.HMACKEY;
 var maxRetryAttempts = process.env.MAXRETRYATTEMPTS
 var pollInterval = process.env.POLLINTERVAL
