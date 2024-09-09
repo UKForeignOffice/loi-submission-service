b80439cc3cdbf236a56b1404eb1070780c652a6b
Simon Moore <s.moore@kainos.com>
2024-09-09 11:46:19 +0100

    FCDO-2120 Update submission service to generate presigned urls at the point of orbit submission

--- server/config/config.js
+++ server/config/config.js
@@ -10,17 +10,21 @@ var edmsHost = process.env.EDMS_HOST;
 var maxRetryAttempts = process.env.MAXRETRYATTEMPTS
 var pollInterval = process.env.POLLINTERVAL
 var submissionApiUrl = process.env.SUBMISSIONAPIURL;
+var nodeEnv = process.env.NODE_ENV || "production";
+var s3Bucket = process.env.S3_BUCKET;
 
 var config = {
-    "additionalPaymentApiUrl": additionalPaymentApiUrl,
+    additionalPaymentApiUrl,
     "db": dbConn,
-    "edmsBearerToken": edmsBearerToken,
-    "edmsHost": edmsHost,
+    edmsBearerToken,
+    edmsHost,
     edmsAuthHost,
     edmsAuthScope,
-    "maxRetryAttempts": maxRetryAttempts,
-    "pollInterval": pollInterval,
-    "submissionApiUrl": submissionApiUrl,
+    maxRetryAttempts,
+    pollInterval,
+    submissionApiUrl,
+    nodeEnv,
+    s3Bucket
 };
 
 module.exports = config;
