9e655e514741d6e2eaa797cce6481e114738a543
Alistair Laing <aliuk2012@users.noreply.github.com>
2026-04-29 13:44:10 +0100

    Fail CI if covereage is below 100%
    Signed-off-by: Alistair Laing <aliuk2012@users.noreply.github.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,14 @@ export default defineConfig({
     exclude: [...configDefaults.exclude],
     coverage: {
       provider: 'v8',
+      all: true,
+      include: ['server/**/*.js', '!server/app.js', '!server/server.js'],
+      thresholds: {
+        lines: 100,
+        functions: 100,
+        branches: 100,
+        statements: 100,
+      },
     },
   },
 })
