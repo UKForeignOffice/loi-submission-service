3239fcdb19bad86fcacb92ab9a0da927ee691ea1
Alistair Laing <aliuk2012@users.noreply.github.com>
2026-04-29 17:00:43 +0100

    Import correct export

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 import { config } from './config/config.js'
-import { checkForApplications } from './controllers/pollForAdditionalPaymentsController.js'
-import { checkForAdditionalPayments } from './controllers/pollForApplicationsController.js'
+import { checkForAdditionalPayments } from './controllers/pollForAdditionalPaymentsController.js'
+import { checkForApplications } from './controllers/pollForApplicationsController.js'
 
 // Create our Express application
 const app = express()
