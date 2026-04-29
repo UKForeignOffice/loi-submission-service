c3827e2bc6ed31cbd669fa42026b693893e708b5
Alistair Laing <aliuk2012@users.noreply.github.com>
2026-04-29 12:18:03 +0100

    Remove Chai, Mocha, Sinon
    These three have been replaced by vitest
    
    Signed-off-by: Alistair Laing <aliuk2012@users.noreply.github.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    exclude: [...configDefaults.exclude, 'test/**'],
+    exclude: [...configDefaults.exclude],
     // coverage: {
     //   provider: 'v8',
     // },
